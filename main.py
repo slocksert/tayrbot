e540dba0b01cba6d82822bd616aeaa5d3334142c
slocksert <rsdlj01@gmail.com>
2024-09-23 21:42:53 -0300

    added try except to prevent errors in the terminal

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from tayrbot import TayrBot
 
-tayrbot = TayrBot()
+try:
+    tayrbot = TayrBot()
 
-email_input, pwd_input, login_btn = tayrbot.find_login_inputs()
-tayrbot.interact_with_login_inputs(email_input, pwd_input, login_btn)
+    email_input, pwd_input, login_btn = tayrbot.find_login_inputs()
+    tayrbot.interact_with_login_inputs(email_input, pwd_input, login_btn)
 
-tayrbot.close_ad()
-tayrbot.find_menu_icon_and_move_through()
-tayrbot.switch_to_iframe_and_submit()
+    tayrbot.close_ad()
+    tayrbot.find_menu_icon_and_move_through()
+    tayrbot.switch_to_iframe_and_submit()
 
-tayrbot.run()
-tayrbot.driver.quit()
+    tayrbot.run()
+    tayrbot.driver.quit()
+except KeyboardInterrupt:
+    tayrbot.driver.quit()
+    quit()
+except Exception as e:
+    print(e)
